96a305e90268269b8e122adda5d4130a57512ce2
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-10-01 00:10:14 +0900

    example

---
+++ 2주차/DOM접근.js
@@ -0,0 +1,17 @@
+var domid = document.getElementById('buttonid');
+domid.addEventListener('click',function(){
+  if(domid.value == "버튼클릭함!"){
+    domid.value = "버튼";
+  }else{
+    domid.value = "버튼클릭함!"
+  }
+
+});
+var domclass = document.getElementsByClassName('buttonclass')[0];
+
+domclass.addEventListener('mouseover',function(){
+  domclass.style.background = "red";
+});
+domclass.addEventListener('mouseout',function(){
+  domclass.style.background = "white";
+})
