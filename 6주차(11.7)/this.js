67455e5a8aaabbfd14c4f9b1a18eec735ca0f063
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-11-04 23:43:02 +0900

    1104

--- 6주차(11.7)/this.js
+++ 6주차(11.7)/this.js
@@ -21,3 +21,18 @@ function Hello(a){
 }
 var ex3 = new Hello(12);
 console.log(ex3.a);  //12
+
+
+// 문제
+var ex1 = 1;
+var ex = {
+    a:function(){
+        var ex1 = 123;
+        function hello(){
+            console.log(this.ex1); // 메서드 안의 "일반함수" 에 있으므로 전역에 바인딩.
+        }
+        hello();
+    }
+}
+ex.a();
+// 결과: 1
