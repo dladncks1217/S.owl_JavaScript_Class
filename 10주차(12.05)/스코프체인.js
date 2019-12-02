9dab8db4aaeefdd0f38ef07e66d7d9fbe18e8a05
dlaxodud1217@gmail.com <dlaxodud1217@naver.com>
2019-12-02 18:51:29 +0900

    1202

--- 10주차(12.05)/스코프체인.js
+++ 10주차(12.05)/스코프체인.js
@@ -2,8 +2,9 @@ function outerFunc(){
     var local = 8;
     function innerFunc(innerArg){
         console.log(innerArg+local);
+        console.log("이미 생명주기가 끝난 외부 함수의 변수를 참조하는");
+        console.log("나는 클로저 함수");
     }
     return innerFunc(2);
 }
 outerFunc();
-
